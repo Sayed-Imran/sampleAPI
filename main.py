01ec95004ff79324af3a7b030a1cdcb81646c6be
Sayed-Imran <sayedimran00786@gmail.com>
2023-08-08 19:02:03 +0530

    Rendering collage

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from jinja2 import FileSystemLoader, Environment
+from starlette.responses import StreamingResponse
 import httpx
+from dotenv import load_dotenv
+load_dotenv()
 from schema import Image
 from typing import List
-from dotenv import load_dotenv
 from config import API_Key
-
+import requests
+from PIL import Image
+from io import BytesIO
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/{query}")
+async def collage(request: Request,query:str):
+    
+    url = "https://api.unsplash.com/search/photos"
+    headers = {
+        "Authorization": f"Client-ID {API_Key.unsplash_access_key}"
+    }
+    params = {
+        "query": query
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers, params=params)
+        response_data = response.json()
+    env = Environment(loader=FileSystemLoader("."), autoescape=True)
+    template = env.get_template("templates/index.html.j2")
+
+    variables = {
+        f"IMAGE_{index + 1}": json_obj['urls']['full']
+        for index, json_obj in enumerate(response_data.get("results")[:-1])
+    }
+    print(variables)
+    with open("templates/index.html", "w") as fh:
+        fh.write(template.render(variables))
+    return templates.TemplateResponse("index.html", {"request": request})
+    
 
 @app.get("/search-photos", response_model=List)
 async def search_photos(query: str):
@@ -24,6 +58,3 @@ async def search_photos(query: str):
         response_data = response.json()
     print(response_data.keys())
     return [Image(**data) for data in response_data.get("results")]
-
-if __name__ == "__main__":
-    load_dotenv()
