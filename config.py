9f49d221b1d7d07b6188841fe07e6e6849649837
Sayed-Imran <sayedimran00786@gmail.com>
2023-12-01 15:31:54 +0530

    fix: API KEY

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from pydantic_settings import BaseSettings
 
 
 class _APIKey(BaseSettings):
-    unsplash_access_key: str
+    unsplash_access_key: str = "3pw90nMVGtYg1rzFe3tQisIvdgLBxpi8nEq1KkKNlmM"
 
 API_Key = _APIKey()
